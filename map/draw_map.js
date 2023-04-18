3e94639d7d8da56caeeeacd3ab7c47cffdab1bd9
felipeViana <felipe.140492@gmail.com>
2023-04-18 17:48:55 -0300

    using geoJSON to draw map

--- map/draw_map.js
+++ map/draw_map.js
@@ -1,32 +1,6 @@
-// function to draw district polygons
-// map: map object to draw into
-// map_json: json data with map coordinates
-function drawPolygons(map, map_json) {
-  map_json.features.forEach((district) => {
-    let originalCoordinates = district.geometry.coordinates[0][0];
-
-    let invertedCoordinates = [];
-    originalCoordinates.forEach((coordinate) => {
-      invertedCoordinates.push([coordinate[1], coordinate[0]]);
-    });
-
-    let polygon = L.polygon(invertedCoordinates).addTo(map);
-
-    let districtInfo = district.properties;
-
-    polygon.bindPopup(
-      districtInfo.name +
-        ", setor: " +
-        districtInfo.setor +
-        ", zona: " +
-        districtInfo.zona
-    );
-  });
-}
-
 // create map object
 // TODO: get center of coordinates
-let map = L.map("map").setView([-23.2, -45.9], 13);
+let map = L.map("map").setView([-23.22, -45.91], 13);
 
 // add tileLayer to map
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -35,17 +9,18 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-// fetch data and draw district polygons
+// fetch data and draw features from geoJSON
 if (localStorage.getItem("map_json") === null) {
   console.log("going to fetch data ..");
   fetch("./../data/geometrias_bairros.json")
     .then((r) => r.json())
     .then((json) => {
       localStorage.setItem("map_json", JSON.stringify(json));
-      drawPolygons(map, json);
+      L.geoJSON(json).addTo(map);
     });
+  // TODO: handle fetch error
 } else {
   console.log("data already fetched");
   let map_json = JSON.parse(localStorage.getItem("map_json"));
-  drawPolygons(map, map_json);
+  L.geoJSON(map_json).addTo(map);
 }
